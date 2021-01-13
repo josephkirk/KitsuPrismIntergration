8aedfdda8e8b3ab9d4c250eaa07758fa7e358f77
Nguyen Phi Hung <nguyenphihung.tech@outlook.com>
2021-01-13 19:10:37 +0700

    add kitsu export

---
+++ Scripts/StateManagerNodes/kitsu_Export.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+####################################################
+#
+# PRISM - Pipeline for animation and VFX projects
+#
+# www.prism-pipeline.com
+#
+# contact: contact@prism-pipeline.com
+#
+####################################################
+#
+#
+# Copyright (C) 2016-2020 Richard Frangenberg
+#
+# Licensed under GNU GPL-3.0-or-later
+#
+# This file is part of Prism.
+#
+# Prism is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Prism is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Prism.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import os
+import sys
+import time
+import traceback
+import platform
+
+try:
+    from PySide2.QtCore import *
+    from PySide2.QtGui import *
+    from PySide2.QtWidgets import *
+except:
+    from PySide.QtCore import *
+    from PySide.QtGui import *
+
+import hou
+
+from PrismUtils.Decorators import err_catcher as err_catcher
+
+
+class ExportClass(object):
+    className = "Kitsu_Export"
+    listType = "Export"
+
+    @err_catcher(name=__name__)
+    def setup(self, state, core, stateManager, node=None, stateData=None):
+        self.state = state
+        self.core = core
+        self.stateManager = stateManager
+
+        self.e_name.setText(state.text(0))
+
+        self.node = None
+        self.curCam = None
+        self.initsim = True
